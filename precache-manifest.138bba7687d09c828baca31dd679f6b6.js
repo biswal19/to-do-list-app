29498554f9f4bc6d283f2fe109378193eeaf37cc
chandrika <chandrikabiswal1@gmail.com>
2020-04-17 12:26:58 +0530

    Updates

---
+++ precache-manifest.138bba7687d09c828baca31dd679f6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354f6c3cae815f4a3648f13967b505be",
+    "url": "/to-do-list-app-react/index.html"
+  },
+  {
+    "revision": "c62a9eb361532242dc8a",
+    "url": "/to-do-list-app-react/static/css/main.7e0068c6.chunk.css"
+  },
+  {
+    "revision": "a095dfea5f974d9e87bb",
+    "url": "/to-do-list-app-react/static/js/2.05728804.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-app-react/static/js/2.05728804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62a9eb361532242dc8a",
+    "url": "/to-do-list-app-react/static/js/main.d51d3194.chunk.js"
+  },
+  {
+    "revision": "5ddf99052598aca17244",
+    "url": "/to-do-list-app-react/static/js/runtime-main.0d30cb92.js"
+  }
+]);
